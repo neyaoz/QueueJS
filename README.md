f64e4db1fad7126d1ecccc02dfd061fc66dce5f7
Necati Özal <posta@necatiozal.com>
2014-10-25 22:38:20 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+Queue JS
+========
+
+Queue JS is a simple library which can call your methods in a order you specify. You can make a queue with related methods, and start it whenever you want. It will call your methods one by one until complete. As you see, it's a basically kind of domino effect that can be controlled.
+
+###Examples
+
+```js
+    function readData(file) {
+        $.get(file, function() {
+            this.run();
+        });
+    };
+
+    var queue  = new Queue();
+
+        queue.add(readData, "example-1.json");
+
+        queue.add(function() {
+            console.log("Call me.");
+            this.run();
+        });
+
+        queue.add(function() {
+            console.log("Call me too.");
+            this.run();
+        });
+
+```
---
+++ queue.js
@@ -0,0 +1,58 @@
+"use strict";
+
+(function() {
+    var
+    Quuee = function() {
+    };
+    Queue.prototype = {
+        index: 0,
+        length: 0,
+
+        add: function(callback, argArray) {
+            this.push([].slice.call(arguments));
+        },
+
+        put: function(callback, argArray) {
+            this.splice(this.index, 0, [].slice.call(arguments));
+        },
+
+        run: function() {
+            var item;
+            if( this.index in this ) {
+                item = this[this.index++];
+                return item[0].apply(this, item[1]);
+            }
+        },
+
+        wait: function(ms) {
+            this.add(function() {
+                window.setTimeout(this.run.bind(this), ms);
+            });
+        },
+
+        skip: function(count) {
+            this.add(function() {
+                this.index += count;
+                this.run();
+            });
+        },
+
+        reset: function() {
+            this.index = 0;
+        },
+
+        toArray: function() {
+            return [].slice.call(this);
+        },
+
+        //Behaves like an Array's method
+        push: [].push, sort: [].sort, splice: [].splice
+    };
+
+    if(typeof define === "function" && define.amd)
+        define(function() { return Queue; });
+    else if(typeof module === "object" && module.exports)
+        module.exports = Queue;
+    else
+        this.Queue = Queue;
+})();
